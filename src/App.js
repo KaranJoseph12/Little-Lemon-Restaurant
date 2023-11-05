b9dd4cd903c3c2ec957456b496bc11676a209e92
Karan <karanabrahamjoseph12@gmail.com>
2023-11-05 16:23:33 +0530

    hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Nav from "./components/Nav";
 import "./App.css"
+import Header from "./components/Header";
+
 
 function App() {
 
 return(
   <>
     <Nav/>
+    <Header/>
   </>
 )
 
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import main from "../images/Main.jpg"
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <section>
+        {/* banner texts */}
+        <div>
+          <h2>Little Lemon</h2>
+          <h3>Chicago</h3>
+          <p>We are a family owned Mediterraneran Restaurant, focused on traditional recipes served with a modern twist</p>
+          <button aria-label='On Click'>Reserve Table</button>
+        </div>
+        {/* image banner */}
+        <div className='banner-img'>
+          <img src={main} alt="img Banner" />
+        </div>
+ 
+      </section>
+
+    </header>
+  )
+}
+
+export default Header
