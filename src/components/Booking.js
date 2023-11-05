3216f4b056525b41d42e8c2cbfd4fd251d39e99b
Karan <karanabrahamjoseph12@gmail.com>
2023-11-05 19:00:42 +0530

    confirmed page error fixed

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -3,7 +3,7 @@ import BookingForm from './BookingForm'
 
 const Booking = (props) => {
   return (
-    <BookingForm availableTimes ={props.availableTimes} dispatch={props.dispatch} SubmitForm = {props.SubmitForm} />
+    <BookingForm availableTimes ={props.availableTimes} dispatch={props.dispatch} submitForm = {props.submitForm} />
   )
 }
 
